f6143bcd35102042613d65ae1f385b63b5718752
Justin Chrysler <pintodragon@gmail.com>
2024-07-14 19:42:45 -0400

    Added all races starting mounts

--- packs/mcrilly/better-starting-mount/output/Better_Starting_Mount-1.0.0-creature_questender.sql
+++ packs/mcrilly/better-starting-mount/output/Better_Starting_Mount-1.0.0-creature_questender.sql
@@ -20,8 +20,160 @@ VALUES (
 -- EOF creature_questender
 -- START creature_questender
 SET
+@id := 7954,
+@quest := 91100002;
+
+DELETE FROM creature_questender WHERE
+    id=@id AND
+    quest=@quest
+;
+
+INSERT INTO creature_questender (
+    `id`,
+    `quest`
+)
+VALUES (
+    @id,
+    @quest
+);
+-- EOF creature_questender
+-- START creature_questender
+SET
+@id := 4753,
+@quest := 91100003;
+
+DELETE FROM creature_questender WHERE
+    id=@id AND
+    quest=@quest
+;
+
+INSERT INTO creature_questender (
+    `id`,
+    `quest`
+)
+VALUES (
+    @id,
+    @quest
+);
+-- EOF creature_questender
+-- START creature_questender
+SET
+@id := 4772,
+@quest := 91100004;
+
+DELETE FROM creature_questender WHERE
+    id=@id AND
+    quest=@quest
+;
+
+INSERT INTO creature_questender (
+    `id`,
+    `quest`
+)
+VALUES (
+    @id,
+    @quest
+);
+-- EOF creature_questender
+-- START creature_questender
+SET
+@id := 20914,
+@quest := 91100005;
+
+DELETE FROM creature_questender WHERE
+    id=@id AND
+    quest=@quest
+;
+
+INSERT INTO creature_questender (
+    `id`,
+    `quest`
+)
+VALUES (
+    @id,
+    @quest
+);
+-- EOF creature_questender
+-- START creature_questender
+SET
 @id := 4752,
 @quest := 91100001;
+
+DELETE FROM creature_questender WHERE
+    id=@id AND
+    quest=@quest
+;
+
+INSERT INTO creature_questender (
+    `id`,
+    `quest`
+)
+VALUES (
+    @id,
+    @quest
+);
+-- EOF creature_questender
+-- START creature_questender
+SET
+@id := 4773,
+@quest := 91100006;
+
+DELETE FROM creature_questender WHERE
+    id=@id AND
+    quest=@quest
+;
+
+INSERT INTO creature_questender (
+    `id`,
+    `quest`
+)
+VALUES (
+    @id,
+    @quest
+);
+-- EOF creature_questender
+-- START creature_questender
+SET
+@id := 3690,
+@quest := 91100007;
+
+DELETE FROM creature_questender WHERE
+    id=@id AND
+    quest=@quest
+;
+
+INSERT INTO creature_questender (
+    `id`,
+    `quest`
+)
+VALUES (
+    @id,
+    @quest
+);
+-- EOF creature_questender
+-- START creature_questender
+SET
+@id := 7953,
+@quest := 91100008;
+
+DELETE FROM creature_questender WHERE
+    id=@id AND
+    quest=@quest
+;
+
+INSERT INTO creature_questender (
+    `id`,
+    `quest`
+)
+VALUES (
+    @id,
+    @quest
+);
+-- EOF creature_questender
+-- START creature_questender
+SET
+@id := 16280,
+@quest := 91100009;
 
 DELETE FROM creature_questender WHERE
     id=@id AND
--- packs/mcrilly/better-starting-mount/output/Better_Starting_Mount-1.0.0-quest_offer_reward.sql
+++ packs/mcrilly/better-starting-mount/output/Better_Starting_Mount-1.0.0-quest_offer_reward.sql
@@ -47,6 +47,190 @@ VALUES (
 -- EOF quest_offer_reward
 -- START quest_offer_reward
 SET
+@id := 91100002,
+@emote1 := 0,
+@emote2 := 0,
+@emote3 := 0,
+@emote4 := 0,
+@emotedelay1 := 0,
+@emotedelay2 := 0,
+@emotedelay3 := 0,
+@emotedelay4 := 0,
+@rewardtext := "Looks like you're some kind of great hero the High Tinker thinks highly of. I was ordered to give you this mount. Hope it helps, but don't think you'll get riding lessons for free. I gotta charge something, hero or not.
+",
+@verifiedbuild := 0;
+
+DELETE FROM quest_offer_reward WHERE
+    id=@id
+;
+
+INSERT INTO quest_offer_reward (
+    `id`,
+    `emote1`,
+    `emote2`,
+    `emote3`,
+    `emote4`,
+    `emotedelay1`,
+    `emotedelay2`,
+    `emotedelay3`,
+    `emotedelay4`,
+    `rewardtext`,
+    `verifiedbuild`
+)
+VALUES (
+    @id,
+    @emote1,
+    @emote2,
+    @emote3,
+    @emote4,
+    @emotedelay1,
+    @emotedelay2,
+    @emotedelay3,
+    @emotedelay4,
+    @rewardtext,
+    @verifiedbuild
+);
+-- EOF quest_offer_reward
+-- START quest_offer_reward
+SET
+@id := 91100003,
+@emote1 := 0,
+@emote2 := 0,
+@emote3 := 0,
+@emote4 := 0,
+@emotedelay1 := 0,
+@emotedelay2 := 0,
+@emotedelay3 := 0,
+@emotedelay4 := 0,
+@rewardtext := "Looks like you're some kind of great hero the Arch Druid thinks highly of. I was ordered to give you this mount. Hope it helps, but don't think you'll get riding lessons for free. I gotta charge something, hero or not.
+",
+@verifiedbuild := 0;
+
+DELETE FROM quest_offer_reward WHERE
+    id=@id
+;
+
+INSERT INTO quest_offer_reward (
+    `id`,
+    `emote1`,
+    `emote2`,
+    `emote3`,
+    `emote4`,
+    `emotedelay1`,
+    `emotedelay2`,
+    `emotedelay3`,
+    `emotedelay4`,
+    `rewardtext`,
+    `verifiedbuild`
+)
+VALUES (
+    @id,
+    @emote1,
+    @emote2,
+    @emote3,
+    @emote4,
+    @emotedelay1,
+    @emotedelay2,
+    @emotedelay3,
+    @emotedelay4,
+    @rewardtext,
+    @verifiedbuild
+);
+-- EOF quest_offer_reward
+-- START quest_offer_reward
+SET
+@id := 4772,
+@emote1 := 0,
+@emote2 := 0,
+@emote3 := 0,
+@emote4 := 0,
+@emotedelay1 := 0,
+@emotedelay2 := 0,
+@emotedelay3 := 0,
+@emotedelay4 := 0,
+@rewardtext := "Looks like you're some kind of great hero the King thinks highly of. I was ordered to give you this mount. Hope it helps, but don't think you'll get riding lessons for free. I gotta charge something, hero or not.
+",
+@verifiedbuild := 0;
+
+DELETE FROM quest_offer_reward WHERE
+    id=@id
+;
+
+INSERT INTO quest_offer_reward (
+    `id`,
+    `emote1`,
+    `emote2`,
+    `emote3`,
+    `emote4`,
+    `emotedelay1`,
+    `emotedelay2`,
+    `emotedelay3`,
+    `emotedelay4`,
+    `rewardtext`,
+    `verifiedbuild`
+)
+VALUES (
+    @id,
+    @emote1,
+    @emote2,
+    @emote3,
+    @emote4,
+    @emotedelay1,
+    @emotedelay2,
+    @emotedelay3,
+    @emotedelay4,
+    @rewardtext,
+    @verifiedbuild
+);
+-- EOF quest_offer_reward
+-- START quest_offer_reward
+SET
+@id := 20914,
+@emote1 := 0,
+@emote2 := 0,
+@emote3 := 0,
+@emote4 := 0,
+@emotedelay1 := 0,
+@emotedelay2 := 0,
+@emotedelay3 := 0,
+@emotedelay4 := 0,
+@rewardtext := "Looks like you're some kind of great hero the Prophet thinks highly of. I was ordered to give you this mount. Hope it helps, but don't think you'll get riding lessons for free. I gotta charge something, hero or not.
+",
+@verifiedbuild := 0;
+
+DELETE FROM quest_offer_reward WHERE
+    id=@id
+;
+
+INSERT INTO quest_offer_reward (
+    `id`,
+    `emote1`,
+    `emote2`,
+    `emote3`,
+    `emote4`,
+    `emotedelay1`,
+    `emotedelay2`,
+    `emotedelay3`,
+    `emotedelay4`,
+    `rewardtext`,
+    `verifiedbuild`
+)
+VALUES (
+    @id,
+    @emote1,
+    @emote2,
+    @emote3,
+    @emote4,
+    @emotedelay1,
+    @emotedelay2,
+    @emotedelay3,
+    @emotedelay4,
+    @rewardtext,
+    @verifiedbuild
+);
+-- EOF quest_offer_reward
+-- START quest_offer_reward
+SET
 @id := 91100001,
 @emote1 := 0,
 @emote2 := 0,
@@ -57,6 +241,190 @@ SET
 @emotedelay3 := 0,
 @emotedelay4 := 0,
 @rewardtext := "Looks like you're some kind of great hero the Warchief thinks highly of. They told me to give you this mount. Hope it helps, but don't think you'll get riding lessons for free. I gotta charge something, hero or not.
+",
+@verifiedbuild := 0;
+
+DELETE FROM quest_offer_reward WHERE
+    id=@id
+;
+
+INSERT INTO quest_offer_reward (
+    `id`,
+    `emote1`,
+    `emote2`,
+    `emote3`,
+    `emote4`,
+    `emotedelay1`,
+    `emotedelay2`,
+    `emotedelay3`,
+    `emotedelay4`,
+    `rewardtext`,
+    `verifiedbuild`
+)
+VALUES (
+    @id,
+    @emote1,
+    @emote2,
+    @emote3,
+    @emote4,
+    @emotedelay1,
+    @emotedelay2,
+    @emotedelay3,
+    @emotedelay4,
+    @rewardtext,
+    @verifiedbuild
+);
+-- EOF quest_offer_reward
+-- START quest_offer_reward
+SET
+@id := 91100006,
+@emote1 := 0,
+@emote2 := 0,
+@emote3 := 0,
+@emote4 := 0,
+@emotedelay1 := 0,
+@emotedelay2 := 0,
+@emotedelay3 := 0,
+@emotedelay4 := 0,
+@rewardtext := "Looks like you're some kind of great hero the Banshee Queen thinks highly of. They told me to give you this mount. Hope it helps, but don't think you'll get riding lessons for free. I gotta charge something, hero or not.
+",
+@verifiedbuild := 0;
+
+DELETE FROM quest_offer_reward WHERE
+    id=@id
+;
+
+INSERT INTO quest_offer_reward (
+    `id`,
+    `emote1`,
+    `emote2`,
+    `emote3`,
+    `emote4`,
+    `emotedelay1`,
+    `emotedelay2`,
+    `emotedelay3`,
+    `emotedelay4`,
+    `rewardtext`,
+    `verifiedbuild`
+)
+VALUES (
+    @id,
+    @emote1,
+    @emote2,
+    @emote3,
+    @emote4,
+    @emotedelay1,
+    @emotedelay2,
+    @emotedelay3,
+    @emotedelay4,
+    @rewardtext,
+    @verifiedbuild
+);
+-- EOF quest_offer_reward
+-- START quest_offer_reward
+SET
+@id := 91100007,
+@emote1 := 0,
+@emote2 := 0,
+@emote3 := 0,
+@emote4 := 0,
+@emotedelay1 := 0,
+@emotedelay2 := 0,
+@emotedelay3 := 0,
+@emotedelay4 := 0,
+@rewardtext := "Looks like you're some kind of great hero the High Chieftain thinks highly of. They told me to give you this mount. Hope it helps, but don't think you'll get riding lessons for free. I gotta charge something, hero or not.
+",
+@verifiedbuild := 0;
+
+DELETE FROM quest_offer_reward WHERE
+    id=@id
+;
+
+INSERT INTO quest_offer_reward (
+    `id`,
+    `emote1`,
+    `emote2`,
+    `emote3`,
+    `emote4`,
+    `emotedelay1`,
+    `emotedelay2`,
+    `emotedelay3`,
+    `emotedelay4`,
+    `rewardtext`,
+    `verifiedbuild`
+)
+VALUES (
+    @id,
+    @emote1,
+    @emote2,
+    @emote3,
+    @emote4,
+    @emotedelay1,
+    @emotedelay2,
+    @emotedelay3,
+    @emotedelay4,
+    @rewardtext,
+    @verifiedbuild
+);
+-- EOF quest_offer_reward
+-- START quest_offer_reward
+SET
+@id := 91100008,
+@emote1 := 0,
+@emote2 := 0,
+@emote3 := 0,
+@emote4 := 0,
+@emotedelay1 := 0,
+@emotedelay2 := 0,
+@emotedelay3 := 0,
+@emotedelay4 := 0,
+@rewardtext := "Looks like you're some kind of great hero the Warchief thinks highly of. They told me to give you this mount. Hope it helps, but don't think you'll get riding lessons for free. I gotta charge something, hero or not.
+",
+@verifiedbuild := 0;
+
+DELETE FROM quest_offer_reward WHERE
+    id=@id
+;
+
+INSERT INTO quest_offer_reward (
+    `id`,
+    `emote1`,
+    `emote2`,
+    `emote3`,
+    `emote4`,
+    `emotedelay1`,
+    `emotedelay2`,
+    `emotedelay3`,
+    `emotedelay4`,
+    `rewardtext`,
+    `verifiedbuild`
+)
+VALUES (
+    @id,
+    @emote1,
+    @emote2,
+    @emote3,
+    @emote4,
+    @emotedelay1,
+    @emotedelay2,
+    @emotedelay3,
+    @emotedelay4,
+    @rewardtext,
+    @verifiedbuild
+);
+-- EOF quest_offer_reward
+-- START quest_offer_reward
+SET
+@id := 91100009,
+@emote1 := 0,
+@emote2 := 0,
+@emote3 := 0,
+@emote4 := 0,
+@emotedelay1 := 0,
+@emotedelay2 := 0,
+@emotedelay3 := 0,
+@emotedelay4 := 0,
+@rewardtext := "Looks like you're some kind of great hero the Regent Lord thinks highly of. They told me to give you this mount. Hope it helps, but don't think you'll get riding lessons for free. I gotta charge something, hero or not.
 ",
 @verifiedbuild := 0;
 
